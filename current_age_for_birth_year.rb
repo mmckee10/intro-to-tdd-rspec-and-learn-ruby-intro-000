141db45804d308a5c61f512c6d6cc69863c32c33
Mark T McKee III <mmckee10@gmail.com>
2020-05-17 21:54:23 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,14 +1,3 @@
-require_relative
-'../current_age_for_birth_year.rb'
-describe "current_age_for_birth_year method" do
-  it "returns the age of a person based on their year of birth" do
-    age_of_person =
-    def
-      current_age_for_birth_year (birth_year)
-      2003 - birth_year
-end
-    expect (age_of_person) .to eq(19)
+def 
+  current_age_for_birth_year(birth_year)
   end
-end
-def current_age_for_birth_year
-end
